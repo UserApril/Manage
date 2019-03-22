9754376d461e3e8e9db71324a00f03d0422dc344
zhaozheng <zhaozhengsc@sinosoft.com.cn>
2019-03-22 17:02:35 +0800

    配置修改

--- manage/manage-database-service/src/main/java/com/rongzhi/database/service/DatabaseServiceApplication.java
+++ manage/manage-database-service/src/main/java/com/rongzhi/database/service/DatabaseServiceApplication.java
@@ -1,25 +1,17 @@
 package com.rongzhi.database.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableFeignClients
-@RequestMapping("/get")
+@EnableDiscoveryClient
 public class DatabaseServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DatabaseServiceApplication.class,args);
     }
 
-    @Value("${name}")
-    String name;
-
-    @RequestMapping("/name")
-    public String getName(){
-        return name;
-    }
 }
 
